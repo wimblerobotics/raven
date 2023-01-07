8884f958841c2836ae31bfd59fd8124108a61ddd
Michael Wimble <mike@wimblerobotics.com>
2023-01-06 19:44:09 -0800

    TOF display is working

--- raven_base/scripts/monitor.py
+++ raven_base/scripts/monitor.py
@@ -1,8 +1,15 @@
-
+from datetime import datetime
 import tkinter as tk
 import matplotlib.pyplot as plt
-from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
+from matplotlib.backends.backend_tkagg import (
+    FigureCanvasTkAgg, NavigationToolbar2Tk)
 import numpy as np
+import rclpy
+from rclpy.node import Node
+from rclpy.qos import QoSProfile, QoSReliabilityPolicy, QoSHistoryPolicy
+from sensor_msgs.msg._range import Range
+from std_msgs.msg import String
+from threading import Thread, Lock
 # import tkinter as tk
 # # from tkinter.ttk import *
 # import matplotlib as plt
@@ -10,92 +17,263 @@ import numpy as np
 # from matplotlib.figure import Figure
 # from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
+ros_mutex = Lock()
+
+
+class TofSubscriberNode(Node):
+
+    def __init__(self):
+        super().__init__('tof_subscriber_node')
+        self.number_sensors = 8  # There are 8 sensors.
+        self.start_time = datetime.now()
+        self.callback_count = np.zeros(self.number_sensors, dtype='int32')
+
+        # I want to show the variance over this number of the last readings.
+        self.number_values_to_cache = 20
+        self.last_n_values_per_sensor = np.zeros(
+            (self.number_sensors, self.number_values_to_cache), dtype='float')
+        self.next_index_number = np.zeros((self.number_sensors), dtype='int32')
+
+        # Set up the ROS 2 quality of service in order to read the sensor data.
+        qos_profile = QoSProfile(
+            reliability=QoSReliabilityPolicy.BEST_EFFORT,
+            history=QoSHistoryPolicy.KEEP_LAST,
+            depth=1
+        )
+
+        # Subscribe to the sensor topics.
+        for sensor_number in range(8):
+            self.subscription = self.create_subscription(
+                Range,
+                '/tof{s}Sensor'.format(s=sensor_number),
+                self.listener_callback,
+                qos_profile,
+            )
+
+        self.subscription  # prevent unused variable warning
+
+    # Process a time-of-flight sensor message of type Range.
+
+    def listener_callback(self, msg):
+        global ros_mutex
+        ros_mutex.acquire()
+        # Get the sensor number.
+        sensor_number = int(msg.header.frame_id[-1])
+        range_value = msg.range
+        self.callback_count[sensor_number] = self.callback_count[sensor_number] + 1
+        global tofs
+        tofs.set_value(sensor_number, "%1.3f" % range_value)
+        tofs.set_count(sensor_number, "%d" % self.callback_count[sensor_number])
+        if (range_value > 2.0):
+            # If the range is greater than 2 meters, ignore it by setting it to zero.
+            range_value = 0
+
+        # Capture the last readings of the sensor in a ring buffer.
+        self.last_n_values_per_sensor[sensor_number][self.next_index_number[sensor_number]] = range_value
+
+        if (self.callback_count[sensor_number] % 24) == 0:
+            # Print out the frames per second of sensor data for all 8 sensors since the last plot update.
+            # Divide by 8 if you want to know the frames per second per sensor.
+            duration = datetime.now() - self.start_time
+            fps = self.callback_count[sensor_number] / \
+                (duration.seconds + (duration.microseconds / 1000000.0))
+            tofs.set_rate(sensor_number, "%2.1f" % fps)
+            print("callback_count: %d, duration: %f, fps: %3.2f" % (
+                self.callback_count[sensor_number], (duration.seconds + (duration.microseconds / 1000000.0)), fps))
+
+        # Update the ring buffer index.
+        self.next_index_number[sensor_number] = self.next_index_number[sensor_number] + 1
+        if self.next_index_number[sensor_number] >= self.number_values_to_cache:
+            self.next_index_number[sensor_number] = 0
+
+        ros_mutex.release()
+
+
 class MiscTable:
     def __init__(self, container_panel, container_row, container_col):
         self.x_pad = 6
         self.y_pad = 2
-        self.panel = tk.Frame(container_panel, padx=0, pady=0, highlightbackground="black", highlightthickness=2)
+        self.panel = tk.Frame(container_panel, padx=0, pady=0,
+                              highlightbackground="black", highlightthickness=2)
         self.panel.grid(row=container_row, column=container_col, sticky="n")
-        tk.Label(self.panel, text=f"clock", width=7, padx=self.x_pad, pady=self.y_pad, borderwidth=1, relief="solid", font="Courier 12 bold", anchor="center").grid(row=0, column=0)
-        tk.Label(self.panel, text=f"odom", width=7, padx=self.x_pad, pady=self.y_pad, borderwidth=1, relief="solid", font="Courier 12 bold", anchor="center").grid(row=1, column=0)
-        self.clock =tk.Label(self.panel, text="123456789.123", width=18, padx=self.x_pad, pady=self.y_pad, borderwidth=1, relief="solid", font="Courier 12", anchor="e")
+        tk.Label(self.panel, text=f"clock", width=7, padx=self.x_pad, pady=self.y_pad, borderwidth=1,
+                 relief="solid", font="Courier 12 bold", anchor="center").grid(row=0, column=0)
+        tk.Label(self.panel, text=f"odom", width=7, padx=self.x_pad, pady=self.y_pad, borderwidth=1,
+                 relief="solid", font="Courier 12 bold", anchor="center").grid(row=1, column=0)
+        self.clock = tk.Label(self.panel, text="123456789.123", width=18, padx=self.x_pad,
+                              pady=self.y_pad, borderwidth=1, relief="solid", font="Courier 12", anchor="e")
         self.clock.grid(row=0, column=1)
-        self.odom =tk.Label(self.panel, text="-123.45, -1234.45", width=18, padx=self.x_pad, pady=self.y_pad, borderwidth=1, relief="solid", font="Courier 12", anchor="e")
+        self.odom = tk.Label(self.panel, text="-123.45, -1234.45", width=18, padx=self.x_pad,
+                             pady=self.y_pad, borderwidth=1, relief="solid", font="Courier 12", anchor="e")
         self.odom.grid(row=1, column=1)
 
+
 class ProximityTable:
+    def set_count(obj, which, value):
+        obj.counts[which].config(text=str(value))
+
+    def set_rate(obj, which, value):
+        obj.rates[which].config(text=str(value))
+
     def set_value(obj, which, value):
         obj.values[which].config(text=str(value))
-
+    
     def __init__(self, container_panel, container_row, container_col, name, rows):
         self.name = name
         self.rows = rows
         self.x_pad = 6
         self.y_pad = 2
-        self.panel = tk.Frame(container_panel, padx=0, pady=0, highlightbackground="black", highlightthickness=2)
+        self.panel = tk.Frame(container_panel, padx=0, pady=0,
+                              highlightbackground="black", highlightthickness=2)
         self.panel.grid(row=container_row, column=container_col, sticky="n")
         self.counts = []
         self.rates = []
         self.values = []
-        tk.Label(self.panel, text=f"{name}#", width=7, padx=self.x_pad, pady=self.y_pad, borderwidth=1, relief="solid", font="Courier 12 bold", anchor="center").grid(row=0, column=0)
-        tk.Label(self.panel, text="count", width=7, padx=self.x_pad, pady=self.y_pad, borderwidth=1, relief="solid", font="Courier 12 bold", anchor="e").grid(row=0, column=1)
-        tk.Label(self.panel, text="rate", width=6, padx=self.x_pad, pady=self.y_pad, borderwidth=1, relief="solid", font="Courier 12 bold", anchor="e").grid(row=0, column=2)
-        tk.Label(self.panel, text="range", width=8, padx=self.x_pad, pady=self.y_pad, borderwidth=1, relief="solid", font="Courier 12 bold", anchor="e").grid(row=0, column=3)
+        tk.Label(self.panel, text=f"{name}#", width=7, padx=self.x_pad, pady=self.y_pad, borderwidth=1,
+                 relief="solid", font="Courier 12 bold", anchor="center").grid(row=0, column=0)
+        tk.Label(self.panel, text="count", width=7, padx=self.x_pad, pady=self.y_pad,
+                 borderwidth=1, relief="solid", font="Courier 12 bold", anchor="e").grid(row=0, column=1)
+        tk.Label(self.panel, text="rate", width=6, padx=self.x_pad, pady=self.y_pad,
+                 borderwidth=1, relief="solid", font="Courier 12 bold", anchor="e").grid(row=0, column=2)
+        tk.Label(self.panel, text="range", width=8, padx=self.x_pad, pady=self.y_pad,
+                 borderwidth=1, relief="solid", font="Courier 12 bold", anchor="e").grid(row=0, column=3)
         for i in range(rows):
-            tk.Label(self.panel, text=f"{i}", width=7, padx=self.x_pad, pady=self.y_pad, borderwidth=1, relief="solid", font="Courier 12 bold", anchor="center").grid(row=i+1, column=0)
-            tmp =tk.Label(self.panel, text="1234", width=7, padx=self.x_pad, pady=self.y_pad, borderwidth=1, relief="solid", font="Courier 12", anchor="e")
+            tk.Label(self.panel, text=f"{i}", width=7, padx=self.x_pad, pady=self.y_pad, borderwidth=1,
+                     relief="solid", font="Courier 12 bold", anchor="center").grid(row=i+1, column=0)
+            tmp = tk.Label(self.panel, text="1234", width=7, padx=self.x_pad, pady=self.y_pad,
+                           borderwidth=1, relief="solid", font="Courier 12", anchor="e")
             self.counts.append(tmp)
             tmp.grid(row=i+1, column=1)
-            tmp = tk.Label(self.panel, text="25.4", width=6, padx=self.x_pad, pady=self.y_pad, borderwidth=1, relief="solid", font="Courier 12", anchor="e")
+            tmp = tk.Label(self.panel, text="25.4", width=6, padx=self.x_pad, pady=self.y_pad,
+                           borderwidth=1, relief="solid", font="Courier 12", anchor="e")
             self.rates.append(tmp)
             tmp.grid(row=i+1, column=2)
-            tmp = tk.Label(self.panel, text="1.234", width=8, padx=self.x_pad, pady=self.y_pad, borderwidth=1, relief="solid", font="Courier 12", anchor="e")
+            tmp = tk.Label(self.panel, text="1.234", width=8, padx=self.x_pad,
+                           pady=self.y_pad, borderwidth=1, relief="solid", font="Courier 12", anchor="e")
             self.values.append(tmp)
             tmp.grid(row=i+1, column=3)
 
-def create_circle(x, y, r, canvas): #center coordinates, radius
+
+def create_circle(x, y, r, canvas):  # center coordinates, radius
     x0 = x - r
     y0 = y - r
     x1 = x + r
     y1 = y + r
     return canvas.create_oval(x0, y0, x1, y1, fill="#004")
 
-root = tk.Tk()
-root.geometry("1024x1024")
+# See: https://stackoverflow.com/questions/16745507/tkinter-how-to-use-threads-to-preventing-main-event-loop-from-freezing
+class BackgroundTask():
 
-btn = tk.Label(root, text='Raven data visualization', font='Helvetica 18 bold')
-btn.grid(row=0, column=0, padx=0, pady=0)
+    def __init__( self, taskFuncPointer ):
+        self.__taskFuncPointer_ = taskFuncPointer
+        self.__workerThread_ = None
+        self.__isRunning_ = False
 
-lidar_panel = tk.Frame(root, highlightbackground="black", highlightthickness=1)
-lidar_panel.grid(row=1, column=0)
+    def taskFuncPointer( self ) : return self.__taskFuncPointer_
+
+    def isRunning( self ) : 
+        return self.__isRunning_ and self.__workerThread_.isAlive()
+
+    def start( self ): 
+        if not self.__isRunning_ :
+            self.__isRunning_ = True
+            self.__workerThread_ = self.WorkerThread( self )
+            self.__workerThread_.start()
+
+    def stop( self ) : self.__isRunning_ = False
+
+    class WorkerThread(Thread ):
+        def __init__( self, bgTask ):      
+            Thread.__init__( self )
+            self.__bgTask_ = bgTask
+
+        def run( self ):
+            try :
+                self.__bgTask_.taskFuncPointer()()
+            except Exception as e:
+                print(e)
+            self.__bgTask_.stop()
+
+def tof_thread():
+    global global_args
+    rclpy.init(args=global_args)
+    tof_subscriber = TofSubscriberNode()
+    rclpy.spin(tof_subscriber)
+
+# def ui_thread():
+#     root = tk.Tk()
+#     root.geometry("1024x1024")
+
+#     btn = tk.Label(root, text='Raven data visualization',
+#                    font='Helvetica 18 bold')
+#     btn.grid(row=0, column=0, padx=0, pady=0)
+
+#     lidar_panel = tk.Frame(
+#         root, highlightbackground="black", highlightthickness=1)
+#     lidar_panel.grid(row=1, column=0)
+
+#     map_canvas = tk.Canvas(lidar_panel, width=1024, height=512)
+#     map_canvas.grid(row=0, column=0)
+#     create_circle((1024/2)-54, (512/2)-54, 2, map_canvas)
+#     create_circle((1024/2)-54, (512/2)+54, 2, map_canvas)
+#     # map_canvas.create_polygon(60,40, 30,50, 30,30, 60,40, fill="#ffa500")
+#     map_canvas.create_arc((1024/2)-50, (512/2)-50, (1024/2)+50, (512/2)+50,
+#                           start=173, extent=7, fill="#ffa500", style=tk.PIESLICE)
+
+#     proximity_panel = tk.Frame(master=root)
+#     proximity_panel.grid(row=2, column=0)
+
+#     global tofs, sonars
+#     tofs = ProximityTable(proximity_panel, 0, 0, "tof", 8)
+#     sonars = ProximityTable(proximity_panel, 0, 2, "sonar", 4)
+#     sonars.set_value(1, 2.345)
+
+#     misc = MiscTable(proximity_panel, 0, 3)
+
+#     root.mainloop()
+
+def main(args=None):
+    global global_args
+    global_args = args
+    bg_thread = BackgroundTask(tof_thread)
+    root = tk.Tk()
+    root.geometry("1024x1024")
+
+    btn = tk.Label(root, text='Raven data visualization',
+                   font='Helvetica 18 bold')
+    btn.grid(row=0, column=0, padx=0, pady=0)
+
+    lidar_panel = tk.Frame(
+        root, highlightbackground="black", highlightthickness=1)
+    lidar_panel.grid(row=1, column=0)
+
+    map_canvas = tk.Canvas(lidar_panel, width=1024, height=512)
+    map_canvas.grid(row=0, column=0)
+    create_circle((1024/2)-54, (512/2)-54, 2, map_canvas)
+    create_circle((1024/2)-54, (512/2)+54, 2, map_canvas)
+    # map_canvas.create_polygon(60,40, 30,50, 30,30, 60,40, fill="#ffa500")
+    map_canvas.create_arc((1024/2)-50, (512/2)-50, (1024/2)+50, (512/2)+50,
+                          start=173, extent=7, fill="#ffa500", style=tk.PIESLICE)
+
+    proximity_panel = tk.Frame(master=root)
+    proximity_panel.grid(row=2, column=0)
+
+    global tofs, sonars
+    tofs = ProximityTable(proximity_panel, 0, 0, "tof", 8)
+    sonars = ProximityTable(proximity_panel, 0, 2, "sonar", 4)
+    sonars.set_value(1, 2.345)
+
+    misc = MiscTable(proximity_panel, 0, 3)
+
+    bg_thread.start()
+    #Calling mainloop
+    root.mainloop()
+
+    rclpy.spin(minimal_subscriber)
+
+    minimal_subscriber.destroy_node()
+    rclpy.shutdown()
 
 
-# lidar_figure = plt.figure(dpi=100)
-# lidar_plot = lidar_figure.add_subplot(1, 1, 1)
-# lidar_plot.plot(0.5, 0.3, color="#C41E3A", marker="o", linestyle="")
-# x = [ 0.1, 0.2, 0.3 ]
-# y = [ -0.1, -0.2, -0.3 ]
-# lidar_plot.plot(x, y, color="blue", marker=".", linestyle="")	# Plotting points
-# canvas = FigureCanvasTkAgg(lidar_figure, master=lidar_panel)
-# canvas.get_tk_widget().grid(row=1, column=0)
-map_canvas = tk.Canvas(lidar_panel, width=1024, height=512)
-map_canvas.grid(row=0, column=0)
-create_circle((1024/2)-54, (512/2)-54, 2, map_canvas)
-create_circle((1024/2)-54, (512/2)+54, 2, map_canvas)
-# map_canvas.create_polygon(60,40, 30,50, 30,30, 60,40, fill="#ffa500")
-map_canvas.create_arc((1024/2)-50, (512/2)-50, (1024/2)+50,(512/2)+50, start=173, extent=7, fill="#ffa500", style=tk.PIESLICE)
-
-proximity_panel = tk.Frame(master=root)
-proximity_panel.grid(row=2,column=0)
-# toolbar = NavigationToolbar2Tk(canvas, toolbarFrame)
-# btn2 = tk.Label(proximity_panel, text='col1').grid(row=0, column=0, padx=20, pady=10)
-
-tofs = ProximityTable(proximity_panel, 0, 0, "tof", 8)
-sonars = ProximityTable(proximity_panel, 0, 2, "sonar", 4)
-sonars.set_value(1, 2.345)
-
-misc = MiscTable(proximity_panel, 0, 3)
-
-
-# Calling mainloop
-root.mainloop()
+if __name__ == '__main__':
+    main()
